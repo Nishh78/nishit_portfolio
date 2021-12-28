b366ab754fb5cf8d18c8cacec764d4fc6717559e
Nishh78 <49271005+Nishh78@users.noreply.github.com>
2021-12-29 00:10:08 +0530

    Add files via upload

---
+++ precache-manifest.2b4b3f2d730b3d7d3d6e9de9bb107d33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44ba9d44e61e4abdd5d94f5836dc67f",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "893a8b943904784c1374",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4ef1193d94f09538f99b",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "893a8b943904784c1374",
+    "url": "/react-frontend-dev-portfolio/static/js/2.cef46814.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.cef46814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef1193d94f09538f99b",
+    "url": "/react-frontend-dev-portfolio/static/js/main.d9129def.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
